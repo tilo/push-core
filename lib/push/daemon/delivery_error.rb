be3ddc74a64e5b2430a57c4ebfaf3dbe74a230cf
Tilo Sloboda <tilo.sloboda@gmail.com>
2014-03-18 16:17:42 -0700

    adding device_token to Push::DeliverError

--- lib/push/daemon/delivery_error.rb
+++ lib/push/daemon/delivery_error.rb
@@ -2,16 +2,21 @@ module Push
   class DeliveryError < StandardError
     attr_reader :code, :description, :notify
 
-    def initialize(code, message_id, description, source, notify = true)
+    def initialize(code, message_id, description, source, notify = true, device = nil)
       @code = code
       @message_id = message_id
       @description = description
       @source = source
       @notify = notify
+      @device = device
     end
 
     def message
-      "Unable to deliver message #{@message_id}, received #{@source} error #{@code} (#{@description})"
+      device_msg = @device ? "to device #{@device}" : nil
+      [ "Unable to deliver message #{@message_id}", 
+         device_msg ,
+        "received #{@source} error #{@code} (#{@description})" ,
+      ].compact.join(', ')
     end
   end
 end

93f1008c95118be6f05b998832ebbef0ea04e56c
Tilo Sloboda <tsloboda@teslamotors.com>
2014-04-07 12:59:58 -0700

    processing all available notifications in batches

--- lib/push/daemon/feeder.rb
+++ lib/push/daemon/feeder.rb
@@ -25,9 +25,13 @@ module Push
           sweep  # re-queue expired jobs
 
           with_database_reconnect_and_retry(name) do
-            Push::Message.where(:reserved_by => nil).ready_for_delivery.find_in_batches(:batch_size => BATCH_SIZE) do |batch|
-              enqueue_notifications(batch)
-            end
+            begin
+              num_reserved = Push::Message.where(:reserved_by => nil).ready_for_delivery.limit(BATCH_SIZE).update_all(
+                {:reserved_by => us, :reserved_until => Time.now + RESERVATION_TIME }
+              )
+              enqueue_notifications if num_reserved > 0
+
+            end while num_reserved > 0
           end
 
           interruptible_sleep config.push_poll
@@ -54,25 +58,14 @@ module Push
         end
       end
 
-      def self.enqueue_notifications(batch)
+      def self.enqueue_notifications
         begin
           ready_apps = Push::Daemon::App.ready
-          reserved_until = Time.now + RESERVATION_TIME
 
-          # First reserve the notifications for delivery by this instance of the daemon
-          num_reserved = batch.update_all(
-            {:reserved_by => us, :reserved_until => reserved_until},
-            :conditions => {:reserved_by => nil} # make sure noone else beat us to it
-          )
-          # if we succeeded with the reservation, then process the notifications
-          if num_reserved > 0
-            Push::Message.where(:reserved_by => us).find_each do |notification|
-              if ready_apps.include?(notification.app) && (notification.reserved_until >= Time.now)
-                Push::Daemon::App.deliver(notification)
-              end
+          Push::Message.where(:reserved_by => us).find_each do |notification|
+            if ready_apps.include?(notification.app) && (notification.reserved_until >= Time.now)
+              Push::Daemon::App.deliver(notification)
             end
-          else
-            # someone else beat us to it, or there are no notifications to be processed
           end
         rescue StandardError => e
           Push::Daemon.logger.error(e)
